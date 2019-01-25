b0b6798657c16765fef5d29ba3f4e009d48d396d
Mirko <m_novakovic@yahoo.com>
2019-01-25 21:21:02 +0100

    brojevi dodati

--- probni1.py
+++ probni1.py
@@ -3,3 +3,15 @@ for magician in magicians:
     print(magician.title() + ", that was a great trick!")
     print("I can't wait to see your next trick, " + magician.title() + ".\n")
 print("Thank you, everyone. That was a great magic show!")
+
+for value in range(1,5):
+    print(value)
+
+squares = []
+for value in range(1, 11):
+    square = value ** 2
+    squares.append(square)
+print(squares)
+
+numbers = list(range(1,1000001))
+print(sum(numbers))
